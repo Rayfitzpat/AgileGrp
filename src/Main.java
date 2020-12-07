1580121c38c31b9d80fdc115c0f9c51521a49cd6
Ray Fitzpatrick <ray_fitzpatrick@hotmail.com>
2020-12-07 14:26:26 +0000

    Test commit to creat conflict

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int number1 = 1;
-        int number2 = 2;
-        int number3 = 3;
+        int Value = 1;
+        int value= 2;
+        int value = 3;
 
         int j;
     }
