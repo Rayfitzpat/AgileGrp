b8843fdb6ea7c6526d563d72c59fd5e07bde6c57
Yuliia Dovbak <yulia.dovbak@gmail.com>
2020-12-07 14:28:39 +0000

    testing conflict resolution

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int number1 = 1;
-        int number2 = 2;
-        int number3 = 3;
+        int v1 = 1;
+        int v2 = 2;
+        int v3 = 3;
 
         int j;
     }
